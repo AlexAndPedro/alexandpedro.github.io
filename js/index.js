44929b9384393d07983d422b009c80a947b00200
AlexAndPedro <thealexandpedro@gmail.com>
2020-10-22 12:27:07 -0700

    Combined files
    combined comic_generator_script.js and index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
-function randomFunFact(){
-	
+// Function to generate a random fun fact
+
+function randomFunFact(){	
 	
 	var funFactsList = [
 		"Alex Paws was a Christmas present from Pedro's father.",
@@ -28,3 +29,22 @@ function randomFunFact(){
 	document.getElementById("FunFactStatement").innerHTML = statement;
 }
 
+// Function to generate random comic image
+
+function getRandomImage() { 
+
+ARR_LENGTH = 54;
+  // Generate the three comic panel indices randomly
+  var comic1 = Math.floor(Math.random() * ARR_LENGTH) + 1;
+  var comic2 = Math.floor(Math.random() * ARR_LENGTH) + 1;
+  var comic3 = Math.floor(Math.random() * ARR_LENGTH) + 1;
+  
+  
+  // Write them to the document
+  document.getElementById("RandomComic").innerHTML =
+	('<img class="ComicPanel" src="../images/Comic_Image_English/test/Slide' + comic1 +
+	'.png" width="32%" height="auto"> <img class="ComicPanel" src="../images/Comic_Image_English/test/Slide' + comic2 +
+	'.png" width="32%" height="auto"> <img class="ComicPanel" src="../images/Comic_Image_English/test/Slide' + comic3 +
+	'.png" width="32%" height="auto">'
+	);
+}
