8f75ec88287db09c1e05832824565345a56eaa3b
AlexAndPedro <thealexandpedro@gmail.com>
2020-10-23 13:28:10 -0700

    Update csvReader.js

--- html/dictionary/csvReader.js
+++ html/dictionary/csvReader.js
@@ -78,7 +78,7 @@ function callback(dictionary) {
                 document.querySelector('.input-word').innerHTML = inputWord;
                 document.querySelector('.part-of-speech').innerHTML = dictionary[inputWord]['part of speech'];
                 document.querySelector('.definition').innerHTML = dictionary[inputWord].definition;
-				document.querySelector('.etymology').innerHTML = dictionary[inputWord].etymology];
+				document.querySelector('.etymology').innerHTML = dictionary[inputWord].etymology;
             } else {
                 document.querySelector('.input-word').innerHTML = 'Not Found in Dictionary';
                 document.querySelector('.part-of-speech').innerHTML = '';
