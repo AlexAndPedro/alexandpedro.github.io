bfdaa6c1735e7e14f4cebb29b6b33dea23d720d0
AlexAndPedro <thealexandpedro@gmail.com>
2020-10-22 18:43:33 -0700

    Moved rcg to a folder

--- js/index.js
+++ js/index.js
@@ -48,3 +48,30 @@ ARR_LENGTH = 54;
 	'.PNG" width="32%" height="auto">'
 	);
 }
+
+
+
+// Function for Random Comics
+ // Wait for the page to load first
+        window.onload = function() {
+
+          //Get a reference to the link on the page
+          // with an id of "mylink"
+          var a = document.getElementsByClassName("nav-Random");
+
+          //Set code to run when the link is clicked
+          // by assigning a function to "onclick"
+          a.onclick = function() {
+
+            
+			
+			
+
+            //If you don't want the link to actually 
+            // redirect the browser to another page,
+            // "google.com" in our example here, then
+            // return false at the end of this block.
+            // Note that this also prevents event bubbling,
+            // which is probably what we want here, but won't 
+            // always be the case.
+            return false;
