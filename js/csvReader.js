b0628f69251f6677fb56cfb81f16948d4b522412
AlexAndPedro <thealexandpedro@gmail.com>
2020-12-28 12:52:55 -0800

    Update csvReader.js

--- js/csvReader.js
+++ js/csvReader.js
@@ -94,6 +94,8 @@ function callback(dictionary) {
                 document.querySelector(".part-of-speech").innerHTML = "";
                 document.querySelector(".mampula").innerHTML = "";
 				document.querySelector(".etymology").innerHTML = "";
+				// document.getElementById("definition_title").style.display = "none";
+				// document.getElementById("etymology_title").style.display = "none";
 				document.getElementById("mampula_title").style.display = "none";
             }
             
