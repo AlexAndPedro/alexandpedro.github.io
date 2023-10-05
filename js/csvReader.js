ca7fd4c07373caa3b0790340ff67814cf1b30356
AlexAndPedro <thealexandpedro@gmail.com>
2023-10-05 01:06:30 -0700

    Vocab Update 2.2.3

--- js/csvReader.js
+++ js/csvReader.js
@@ -154,7 +154,7 @@ function callback(dictionary) {
 
                     }
 
-                    if (inputWord.toLowerCase() == mampulanWord[i].toLowerCase()){  
+                    if (inputWord.toLowerCase() == mampulanSymbol[i].toLowerCase()){  
                          //If the user input matches one of the words in the MAMPULAN word list
 
                                              // Compiling html code to be sent to Dictionary Display
