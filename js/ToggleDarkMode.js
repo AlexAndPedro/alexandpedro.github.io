c028508b0d22fbfb38e3c2a7ef2f552488260c32
AlexAndPedro <73212448+AlexAndPedro@users.noreply.github.com>
2025-05-28 23:10:09 -0700

    Dark View Update

--- js/ToggleDarkMode.js
+++ js/ToggleDarkMode.js
@@ -4,4 +4,10 @@ function ToggleDarkMode() {
 	elementBody.toggle("dark-mode");
 	var elementGradient = document.getElementsByClassName("gradient")[0].classList;
     elementGradient.toggle("dark-mode");
+	var elementComicTitle = document.getElementsByClassName("ComicTitle")[0].classList;
+    elementComicTitle.toggle("dark-mode");
+	var elementNavigation = document.getElementsByClassName("ComicNavigation")[0].classList;
+    elementNavigation.toggle("dark-mode");
+	var elementContent = document.getElementsByClassName("PageContent")[0].classList;
+    elementContent.toggle("dark-mode");
  }
